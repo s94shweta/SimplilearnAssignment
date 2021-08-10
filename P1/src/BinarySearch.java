3d3bf0d203cbd843ace38456cf39a09035b5ee1f
S94shweta <s94shweta@gmail.com>
2021-08-11 02:20:54 +0530

    Changes have been committed.

---
+++ P1/src/BinarySearch.java
@@ -0,0 +1,38 @@
+
+public class BinarySearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int arr[]= {4,87,34,98,56};
+		
+		int key = 12;
+        int arrlength = arr.length;
+        binarySearch(arr,0,key,arrlength);
+    }
+
+public static void binarySearch(int[] arr, int start, int key, int length){
+
+        int midValue = (start+length)/2;
+        while(start<=length){
+
+            if(arr[midValue]<key){
+
+                start = midValue + 1;
+            } else if(arr[midValue]==key){
+                System.out.println("Element is found at index :"+midValue);
+                break;
+            }else {
+
+                length=midValue-1;
+            }
+            midValue = (start+length)/2;
+        }
+            if(start>length){
+
+                System.out.println("Element is not found");
+            }
+
+}
+
+}
